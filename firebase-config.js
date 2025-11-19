dee42b09a7bcb5f85f138348ec51117e5c6f5011
ggyaer-hue <ggyaer@gmail.com>
2025-11-19 18:34:38 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// Firebase 콘솔에서 가져온 설정값 (cwgauction-8ae37 프로젝트)
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDldhIELEidJQQck4ljtWznalakpXbAGQA",
+  authDomain: "cwgauction-8ae37.firebaseapp.com",
+  projectId: "cwgauction-8ae37",
+  storageBucket: "cwgauction-8ae37.firebasestorage.app",
+  messagingSenderId: "44783149326",
+  appId: "1:44783149326:web:e6321e381f7ffc4864775f",
+  measurementId: "G-48GXGZ32CW"
+};
